9f3b2a3b29672f72434e2dc1ddd5709bb555e6ad
luisjesus <luisjesus.delgado89@gmail.com>
2018-03-01 13:44:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/didact/sharecetasalpha/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.didact.sharecetasalpha;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent().setClass(
+                        getApplicationContext(), InicioActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
