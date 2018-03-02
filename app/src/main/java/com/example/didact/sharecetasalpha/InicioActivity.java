87cce9c9cf994cafd718e7cb177a39ad6e428e1f
luffyandroid <luisjesus.delgado89@gmail.com>
2018-03-02 01:57:26 +0100

    botones inicio, registro, login, mis recetas y buscador hechos

--- app/src/main/java/com/example/didact/sharecetasalpha/InicioActivity.java
+++ app/src/main/java/com/example/didact/sharecetasalpha/InicioActivity.java
@@ -1,7 +1,9 @@
 package com.example.didact.sharecetasalpha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class InicioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
     }
+    public void clickRegistrar(View view) {
+        Intent mainIntent = new Intent().setClass(getApplicationContext(), RegistroActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    public void clickLogin(View view) {
+        Intent mainIntent = new Intent().setClass(getApplicationContext(), LoginActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
 }
